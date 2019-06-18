f13f32caac83fcc22637d982b97337b3fb725b7b
Karly <karlyhoffman@gmail.com>
2019-06-18 06:03:43 -0500

    initial build (Create React App)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          <div className="todo">
+            <div className="checkbox" />
+            <input type="text" value="Todo one" />
+          </div>
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+export default App;
