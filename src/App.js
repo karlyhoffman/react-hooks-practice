5e63cf62047501bd4444ec0ec27c34a8e3939049
Karly <karlyhoffman@gmail.com>
2019-06-18 06:24:27 -0500

    create new todo functionality

--- src/App.js
+++ src/App.js
@@ -26,6 +26,25 @@ function App() {
     }
   ]);
 
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    // insert a new empty todo after the currently selected todo
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => { // set the focus to the new input field
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
   return (
     <div className="app">
       <div className="header">
@@ -36,7 +55,11 @@ function App() {
           {todos.map((todo, i) => (
             <div className="todo">
               <div className="checkbox" />
-              <input type="text" value={todo.content} />
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)}
+              />
             </div>
           ))}
         </ul>
