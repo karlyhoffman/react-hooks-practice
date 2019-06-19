57907cc10f9fd11ba37d08e739c15bae9db7817f
Karly <karlyhoffman@gmail.com>
2019-06-19 08:31:15 -0500

    move todo functionality to new component

--- src/App.js
+++ src/App.js
@@ -1,98 +1,19 @@
 import React, { useState } from 'react';
+import Todo from './components/Todo';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  /* 
-   * SETTING INITIAL STATE:
-   *
-   * When you initialize state using the useState Hook, you define two values: the getter and the setter.
-   * In this example, todos is the state value itself, and setTodos is the function that updates the state value.
-   *
-   * Below is initializing todos to have a default value of an array filled with objects.
-   */
-  const [todos, setTodos] = useState([
-    {
-      content: 'Pickup dry cleaning',
-      isCompleted: true,
-    },
-    {
-      content: 'Get haircut',
-      isCompleted: false,
-    },
-    {
-      content: 'Build a todo app in React',
-      isCompleted: false,
-    }
-  ]);
-
-  function handleKeyDown(e, i) {
-    if (e.key === 'Enter') {
-      createTodoAtIndex(e, i);
-    }
-    if (e.key === 'Backspace' && todos[i].content === '') {
-      e.preventDefault();
-      return removeTodoAtIndex(i);
-    }
-  }
-
-  function createTodoAtIndex(e, i) {
-    const newTodos = [...todos];
-    // insert a new empty todo after the currently selected todo
-    newTodos.splice(i + 1, 0, {
-      content: '',
-      isCompleted: false,
-    });
-    setTodos(newTodos);
-    setTimeout(() => { // set the focus to the new input field
-      document.forms[0].elements[i + 1].focus();
-    }, 0);
-  }
-
-  function updateTodoAtIndex(e, i) {
-    const newTodos = [...todos];
-    newTodos[i].content = e.target.value;
-    setTodos(newTodos);
-  }
-
-  function toggleTodoCompleteAtIndex(i) {
-    const temporaryTodos = [...todos];
-    temporaryTodos[i].isCompleted = !temporaryTodos[i].isCompleted;
-    setTodos(temporaryTodos);
-  }
-
-  function removeTodoAtIndex(i) {
-    if (i === 0 && todos.length === 1) return;
-    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
-    setTimeout(() => {
-      document.forms[0].elements[i - 1].focus();
-    }, 0);
-  }
 
   return (
     <div className="app">
       <div className="header">
         <img src={logo} className="logo" alt="logo" />
       </div>
-      <form className="todo-list">
-        <ul>
-          {todos.map((todo, i) => (
-            <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
-              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
-                {todo.isCompleted && (
-                  <span>&#x2714;</span>
-                )}
-              </div>
-              <input
-                type="text"
-                value={todo.content}
-                onKeyDown={e => handleKeyDown(e, i)}
-                onChange={e => updateTodoAtIndex(e, i)}
-              />
-            </div>
-          ))}
-        </ul>
-      </form>
+      <section>
+        <h1>Todo List</h1>
+        <Todo/>
+      </section>
     </div>
   );
 }
